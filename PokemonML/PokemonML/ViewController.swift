b13f51f91ef1439cb0feeeceb86fd51673d417f2
kingsleychuuu <kingsleychu0730@hotmail.com>
2020-04-26 22:28:47 -0400

    Added model and update interface

--- PokemonML/PokemonML/ViewController.swift
+++ PokemonML/PokemonML/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        setupNavigationBar()
+        setupImage()
     }
+    
+    fileprivate func setupNavigationBar() {
+        let rightBarItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openCamera))
+        navigationItem.rightBarButtonItem = rightBarItem
+    }
+    
+    fileprivate func setupImage() {
+        let image = UIImageView()
+        image.backgroundColor = .red
+        image.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(image)
+        NSLayoutConstraint.activate([
+            image.heightAnchor.constraint(equalToConstant: 100),
+            image.widthAnchor.constraint(equalToConstant: 100),
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            image.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate {
+    @objc func openCamera() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = false
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //
+    }
 }
 
